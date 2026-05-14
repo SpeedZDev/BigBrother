a74d083ae28f238c4e08e8ec291206ee2d561352
SpeedZDev <98675043+SpeedZDev@users.noreply.github.com>
2026-05-14 00:08:33 -0400

    Got A basic Model Working

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from langchain_openai import ChatOpenAI
 load_dotenv()
 
 AgentModel = ChatOpenAI(model="gpt-5.4")
+
+UserQuery = input("Enter A Prompt/Question")
+Response = AgentModel.invoke(UserQuery)
+print(Response)
