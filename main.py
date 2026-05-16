00e4fe6442b58898da0b677df763f4013c82f053
SpeedZDev <98675043+SpeedZDev@users.noreply.github.com>
2026-05-15 21:46:18 -0400

    Add More Tokens

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from langchain_tavily import TavilySearch
 from langchain.agents import create_agent
 from langchain_community.document_loaders import PyMuPDFLoader
 import streamlit as st
+
 load_dotenv()
 
 
 
-AgentModel = ChatOpenAI(model="gpt-5.4", max_completion_tokens=1000, temperature=0.1)
+AgentModel = ChatOpenAI(model="gpt-5.4", max_completion_tokens=2000, temperature=0.1)
 WebSearchTool = TavilySearch(max_result=5, topic="general")
 Tools = [WebSearchTool]
 SystemPrompt = "You are a General AI Information Assistant that Answers Questions and helps Users On general Everday Actives. When Questions are asks you find the *MOST UP TO DATE* sources from the current date regargding the subject and put these sources that use in a neatly fromatted list! When User Uploads documents Scan these doucments and do as the user asks with these documents"
